c04233b27b43c3b54be64480d6b48f8f62658873
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-07 09:07:18 -0400

    completed the main boilerplate to get everything finished

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/api/shorturl', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
