2122fe3686733550dcbc248f366c7008db173793
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-10 18:54:04 -0400

    added middleware functions and logging states of database

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(mongoose.connection.readyState);
 
 const schema = new mongoose.Schema({name: String});
 const Url = mongoose.model('Url', schema);
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -28,7 +30,33 @@ app.get('/api/hello', function(req, res) {
 });
 
 app.post("/api/shorturl/", function(req, res) {
-  
+  const bodyUrl = req.body.url;
+  console.log(bodyUrl);
+  // const urlConfirmation = dns.lookup(urlParser.parse(bodyUrl).hostname, (err, address, family) => {
+  //   if (err) {
+  //     res.json({error: "invalid URL"});
+  //   } else {
+  //     const newUrl = new Url({name: bodyUrl});
+  //     newUrl.save((err, data) => {
+  //       if (err) {
+  //         res.json({error: "invalid URL"});
+  //       } else {
+  //         res.json({original_url: bodyUrl, short_url: data._id});
+  //       }
+  //     });
+  //   }
+  // });
+});
+
+app.get("/api/shorturl/:id", function(req, res) {
+  const id = req.params.id;
+  Url.findOne({_id: id}, (err, data) => {
+    if (!data) {
+      res.json({error: "invalid URL"});
+    } else {
+      res.redirect(data.name);
+    }
+  });
 });
 
 app.listen(port, function() {
